bb0d86826f875409ee7e75893d48849fb779e782
Arjun Bhatnagar <me@arjunb.com>
2013-07-10 16:07:30 -0400

    Added code to clear badge upon opening
    Added code to clear badge

--- MyPlane/AppDelegate.m
+++ MyPlane/AppDelegate.m
@@ -35,6 +35,12 @@
      UIRemoteNotificationTypeAlert |
      UIRemoteNotificationTypeSound];
     
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    if (currentInstallation.badge != 0) {
+        currentInstallation.badge = 0;
+        [currentInstallation saveEventually];
+    }
+    
 //    PFQuery *pushQuery = [PFInstallation query];
 //    [pushQuery whereKey:@"deviceType" equalTo:@"ios"];
 //    
@@ -62,6 +68,7 @@ didReceiveRemoteNotification:(NSDictionary *)userInfo {
 
 }
 
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -81,7 +88,11 @@ didReceiveRemoteNotification:(NSDictionary *)userInfo {
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    if (currentInstallation.badge != 0) {
+        currentInstallation.badge = 0;
+        [currentInstallation saveEventually];
+    }
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
