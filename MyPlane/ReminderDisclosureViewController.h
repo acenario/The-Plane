53ed8aaaac8f4089410c7f888f5a20136a9c84c5
Arjun Bhatnagar <me@arjunb.com>
2013-07-14 12:04:58 -0400

    Began reminder info
    began reminder info

---
+++ MyPlane/ReminderDisclosureViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReminderDisclosureViewController.h
+//  MyPlane
+//
+//  Created by Arjun Bhatnagar on 7/14/13.
+//  Copyright (c) 2013 Acubed Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReminderDisclosureViewController : UITableViewController
+
+@end
---
+++ MyPlane/ReminderObjectViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReminderObjectViewController.h
+//  MyPlane
+//
+//  Created by Arjun Bhatnagar on 7/14/13.
+//  Copyright (c) 2013 Acubed Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReminderObjectViewController : UITableViewController
+
+@end
